f1c1c74f31031217d075ee2e19fc752ea0d00e44
J Sandate <mrharisu@gmail.com>
2017-02-27 15:51:37 -0700

    complete part1 and part2

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-(function() {
-  'use strict';
+$(document).ready(function(){
 
-  // YOUR CODE HERE
-})();
+  $("#stopButton").click(function(){
+          $("#stopLight").toggleClass("stop");
+      });
+  $("#slowButton").click(function(){
+          $("#slowLight").toggleClass("slow");
+      });
+  $("#goButton").click(function(){
+          $("#goLight").toggleClass("go");
+      });
+
+
+
+//  ***** STOP BUTTON LOG ******
+  $("#stopButton").on('mouseenter',function(){
+          console.log('entered STOP button');
+  })
+  $("#stopButton").on('mouseout',function(){
+          console.log('left STOP button');
+  })
+
+//  ***** SLOW BUTTON LOG ******
+  $("#slowButton").on('mouseenter',function(){
+          console.log('entered SLOW button');
+  })
+  $("#slowButton").on('mouseout',function(){
+          console.log('left SLOW button');
+  })
+
+//  ***** GO BUTTON LOG ******
+  $("#goButton").on('mouseenter',function(){
+          console.log('entered GO button');
+  })
+  $("#goButton").on('mouseout',function(){
+          console.log('left GO button');
+  })
+
+
+
+
+});
